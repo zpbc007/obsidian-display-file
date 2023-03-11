8b1320951f9094eb94157cc774e6be7242902928
zhaopeng <719197352@qq.com>
2023-03-11 22:46:22 +0800

    feat: tmp work

--- main.ts
+++ main.ts
@@ -1,137 +1,205 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Component,
+	Editor,
+	MarkdownRenderer,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	EditorSuggest,
+	EditorPosition,
+	EditorSuggestContext,
+	TFile,
+	EditorSuggestTriggerInfo,
+	FileSystemAdapter,
+} from 'obsidian'
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	mySetting: string
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	mySetting: 'default',
+}
+
+async function obsidianMarkdownRenderer(text: string, element: HTMLSpanElement, path: string) {
+	await MarkdownRenderer.renderMarkdown(text, element, path, (null as unknown) as Component)
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: MyPluginSettings
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		await this.loadSettings()
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SampleSettingTab(this.app, this))
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		this.registerMarkdownCodeBlockProcessor('display-file', async (source, el, ctx) => {
+			const sourcePath = source.trim()
+			// 获取源文件
+			const targetFile = this.app.vault
+				.getFiles()
+				.filter((item) => item.path === sourcePath)[0]
+			console.log('targetFile: ', targetFile)
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			// 没找到
+			if (!targetFile) {
+				return
+			}
+
+			const fileContent = await this.app.vault.cachedRead(targetFile)
+
+			let fullPath = ''
+			if (this.app.vault.adapter instanceof FileSystemAdapter) {
+				fullPath = `${this.app.vault.adapter.getBasePath()}/${targetFile.path}`
+			}
+
+			if (fullPath) {
+				const projectEl = el.createEl('a')
+				projectEl.setText('项目')
+				projectEl.addClass('display-file-title')
+				projectEl.href = `vscode://file//Users/zhaopeng/Documents/self-code/algorithm-battle`
+
+				const titleEl = el.createEl('a')
+				titleEl.setText(targetFile.name)
+				titleEl.addClass('display-file-title')
+				titleEl.href = `vscode://file/${fullPath}?workspace=/Users/zhaopeng/Documents/self-code/algorithm-battle`
+			}
+
+			await obsidianMarkdownRenderer(
+				'```typescript\n' + fileContent + '\n```',
+				el.createDiv(),
+				ctx.sourcePath,
+			)
+		})
+
+		this.registerEditorSuggest(new FileSuggest(this.app, new FileCacheStore(this.app)))
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+		await this.saveData(this.settings)
 	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: MyPlugin
 
 	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+		super(app, plugin)
+		this.plugin = plugin
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this
 
-		containerEl.empty();
+		containerEl.empty()
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'zpxxx Settings for my awesome plugin.' })
 
 		new Setting(containerEl)
 			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder('Enter your secret')
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						console.log('Secret: ' + value)
+						this.plugin.settings.mySetting = value
+						await this.plugin.saveSettings()
+					}),
+			)
+	}
+}
+
+interface Suggestion {
+	path: string
+	startPos: EditorPosition
+}
+class FileSuggest extends EditorSuggest<Suggestion> {
+	constructor(private readonly app: App, private readonly fileStore: FileCacheStore) {
+		super(app)
+	}
+
+	getSuggestions(context: EditorSuggestContext): Suggestion[] | Promise<Suggestion[]> {
+		return this.fileStore.getFiles().map<Suggestion>((item) => ({
+			path: item.path,
+			startPos: {
+				...context.start,
+				ch: 0,
+			},
+		}))
+	}
+
+	onTrigger(
+		cursor: EditorPosition,
+		editor: Editor,
+		file: TFile,
+	): EditorSuggestTriggerInfo | null {
+		if (cursor.line - 1 < 0) {
+			return null
+		}
+		const line = editor.getLine(cursor.line - 1)
+		const matched = line.includes('display-file')
+
+		if (!matched) {
+			return null
+		}
+
+		return {
+			start: {
+				...cursor,
+				ch: 0,
+			},
+			end: cursor,
+			query: 'xxx',
+		}
+	}
+
+	renderSuggestion(value: Suggestion, el: HTMLElement): void {
+		const div = el.doc.createElement('div')
+		div.setText(value.path)
+
+		el.appendChild(div)
+	}
+
+	selectSuggestion({ path, startPos }: Suggestion, evt: MouseEvent | KeyboardEvent): void {
+		const endCh = this.context?.editor.getLine(startPos.line).length || 0
+		this.context?.editor.replaceRange(path, startPos, {
+			line: startPos.line,
+			ch: endCh,
+		})
+	}
+}
+
+class FileCacheStore {
+	private fileCache: TFile[] = []
+	constructor(private readonly app: App) {}
+
+	getFiles() {
+		return this.app.vault.getFiles()
+	}
+
+	/**
+	 * TODO: 根据配置，只保存对应目录下的文件
+	 */
+	private updateCache() {
+		this.fileCache = this.app.vault.getFiles()
+	}
+
+	private listenEvent() {
+		const myUpdateCache = this.updateCache.bind(this)
+		this.app.vault.on('create', myUpdateCache)
+		this.app.vault.on('delete', myUpdateCache)
+		this.app.vault.on('rename', myUpdateCache)
 	}
 }
