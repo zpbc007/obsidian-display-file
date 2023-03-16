d71d8d929553cf8d940a2c9a051b735d1bc65213
zhaopeng <719197352@qq.com>
2023-03-16 10:38:30 +0800

    feat: setting

--- src/setting.ts
+++ src/setting.ts
@@ -28,43 +28,73 @@ export class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty()
 
-		containerEl.createEl('h2', { text: 'Display file content settings' })
+		containerEl.createEl('h2', { text: 'Display file content plugin settings' })
 
-		new Setting(containerEl)
-			.setName('Search Directory')
-			.setDesc('The file directory, default to be the root')
-			.addSearch((search) => {
-				new FolderSuggest(this.app, search.inputEl, (path) => {
-					this.plugin.settings.searchDir = path
-				})
-			})
-		// .add
-		// .addDropdown((dropDown) => {
-		// 	dropDown
-		// 		.addOptions({
-		// 			'/a': '/a',
-		// 			'/a/b': '/a/b',
-		// 			'/a/b/c': '/a/b/c',
-		// 		})
-		// 		.setValue(this.plugin.settings.searchDir)
-		// 		.onChange(async (value) => {
-		// 			this.plugin.settings.searchDir = value
-		// 			await this.plugin.saveSettings()
-		// 		})
-		// })
+		this.addTextField({
+			name: 'Search Directory',
+			desc: 'The file directory, default to be the root',
+			placeholder: 'Enter search directory',
+			valueGetter: () => this.plugin.settings.searchDir,
+			onChange: (value) => (this.plugin.settings.searchDir = value),
+		})
+
+		this.addTextField({
+			name: 'Include regex',
+			desc: 'The file will be included',
+			placeholder: 'Enter the regex',
+			valueGetter: () => this.plugin.settings.includeFileRegex || '',
+			onChange: (value) => (this.plugin.settings.includeFileRegex = value),
+		})
+
+		this.addTextField({
+			name: 'Exclude regex',
+			desc: 'The file will be excluded',
+			placeholder: 'Enter the regex',
+			valueGetter: () => this.plugin.settings.excludeFileRegex || '',
+			onChange: (value) => (this.plugin.settings.excludeFileRegex = value),
+		})
 
 		// new Setting(containerEl)
-		// 	.setName('Setting #1')
-		// 	.setDesc("It's a secret")
-		// 	.addText((text) =>
-		// 		text
-		// 			.setPlaceholder('Enter your secret')
-		// 			.setValue(this.plugin.settings.mySetting)
-		// 			.onChange(async (value) => {
-		// 				console.log('Secret: ' + value)
-		// 				this.plugin.settings.mySetting = value
+		// 	.setName('Search Directory')
+		// 	.setDesc('The file directory, default to be the root')
+		// 	.addText((text) => {
+		// 		text.setPlaceholder('Enter search directory')
+		// 			.setValue(this.plugin.settings.searchDir)
+		// 			.onChange(async (directory) => {
+		// 				this.plugin.settings.searchDir = directory
 		// 				await this.plugin.saveSettings()
-		// 			}),
-		// 	)
+		// 			})
+		// 	})
+		// .addSearch((search) => {
+		// 	new FolderSuggest(this.app, search.inputEl, (path) => {
+		// 		this.plugin.settings.searchDir = path
+		// 	})
+		// })
+	}
+
+	private addTextField({
+		name,
+		desc,
+		placeholder,
+		valueGetter,
+		onChange,
+	}: {
+		name: string
+		desc: string
+		placeholder: string
+		valueGetter: () => string
+		onChange: (value: string) => void
+	}) {
+		new Setting(this.containerEl)
+			.setName(name)
+			.setDesc(desc)
+			.addText((text) => {
+				text.setPlaceholder(placeholder)
+					.setValue(valueGetter())
+					.onChange(async (newValue) => {
+						onChange(newValue)
+						await this.plugin.saveSettings()
+					})
+			})
 	}
 }
